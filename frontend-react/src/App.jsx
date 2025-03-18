3c5d5bcd3704dc47aa9699e5f69c71d09296ead0
Alok Kumar <akumar481@rku.ac.in>
2025-03-19 01:04:20 +0530

    project folder added

---
+++ frontend-react/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import Chai from "./Chai";
+
+function App() {
+  
+
+  return (
+  <div>
+    <h1>    <Chai/> 
+    </h1>
+    <h1>chai aur react</h1>
+    <p>test para</p>
+    </div>
+
+  )
+}
+
+export default App
